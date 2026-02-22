1ce1ca6f593034fa1b850ff9931a1a1ff1b286e4
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-23 00:18:57 +0600

    script file is updated

--- scripts/count.js
+++
@@ -1,9 +0,0 @@
-// !Total Jobs Count Section! //
-const job_cards_section = document.getElementById("job_cards_section");
-const job_cards_section_length = job_cards_section.children.length;
-
-const total_job_count = document.getElementById("total_job_count");
-total_job_count.innerText = job_cards_section_length;
-
-const total_job_count1 = document.getElementById("total_job_count1");
-total_job_count1.innerText = job_cards_section_length;
--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,11 @@ const job_status_btn6 = document.getElementById("job_status_btn6");
 const job_status_btn7 = document.getElementById("job_status_btn7");
 const job_status_btn8 = document.getElementById("job_status_btn8");
 
+const job_card1 = document.getElementById("job_card1");
+const default_interview_section = document.getElementById(
+  "default_interview_section",
+);
+
 document
   .getElementById("interview_btn1")
   .addEventListener("click", function () {
@@ -57,9 +62,15 @@ document
   });
 
 // !Rejected Button Section! //
+const rejected_job_section = document.getElementById("rejected_job_section");
+const default_rejected_section = document.getElementById(
+  "default_rejected_section",
+);
 
 document.getElementById("rejected_btn1").addEventListener("click", function () {
   job_status_btn1.innerHTML = `<button class="btn btn-error">Rejected</button>`;
+  //   rejected_job_section.appendChild(job_card1);
+  //   default_rejected_section.classList.add("hidden");
 });
 
 document.getElementById("rejected_btn2").addEventListener("click", function () {
@@ -89,3 +100,30 @@ document.getElementById("rejected_btn7").addEventListener("click", function () {
 document.getElementById("rejected_btn8").addEventListener("click", function () {
   job_status_btn8.innerHTML = `<button class="btn btn-error">Rejected</button>`;
 });
+
+// !Total Jobs Count Section! //
+const job_cards_section = document.getElementById("job_cards_section");
+const job_cards_section_length = job_cards_section.children.length;
+
+const total_job_count = document.getElementById("total_job_count");
+total_job_count.innerText = job_cards_section_length;
+
+const total_job_count1 = document.getElementById("total_job_count1");
+total_job_count1.innerText = job_cards_section_length;
+
+// !Interview Jobs Count Section! //
+
+// !Machine for Button Toggle! //
+function showOnly(id) {
+  const interviewSection = document.getElementById("interview_job_section");
+  interviewSection.classList.add("hidden");
+
+  const jobSection = document.getElementById("job_cards_section");
+  jobSection.classList.add("hidden");
+
+  const rejectSection = document.getElementById("rejected_job_section");
+  rejectSection.classList.add("hidden");
+
+  const selected = document.getElementById(id);
+  selected.classList.remove("hidden");
+}
