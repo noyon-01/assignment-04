3fcd3c034d19c935a06dd25bbe366f906765ffe4
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-23 19:40:41 +0600

    job count btn is founded

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+let all_job_count = document.getElementById("all_job_count");
+let interview_job_count = document.getElementById("interview_job_count");
+let rejected_job_count = document.getElementById("rejected_job_count");
+let available_job_count = document.getElementById("available_job_count");
+
+
+console.log(all_job_count, interview_job_count, rejected_job_count, available_job_count);
